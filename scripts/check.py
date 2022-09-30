e31248161872745567de0b2762c1509dd033121b
Dmitry Marakasov <amdmi3@amdmi3.ru>
2022-09-30 19:10:55 +0300

    Clean up and de-obscure lohit-* font rules

--- scripts/check.py
+++ scripts/check.py
@@ -320,7 +320,7 @@ schema = Schema(
 
 
 features_by_pattern = {
-    '.*800.renames-and-merges/(.|apmod|emacs|ffext|gimpplugins|haskell|lib|libretro|lua|lv2|nextcloud|node|perl|php|python|rhytmbox|texlive|vim|fonts/[^dl])\.yaml': {
+    '.*800.renames-and-merges/(.|apmod|emacs|ffext|gimpplugins|haskell|lib|libretro|lua|lv2|nextcloud|node|perl|php|python|rhytmbox|texlive|vim|fonts/[^d])\.yaml': {
         'sort_field': 'setname',
     },
     '.*850.split-ambiguities/[^cdh]\.yaml': {
